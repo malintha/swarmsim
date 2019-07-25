f7da0c97975a8ed88626450d6ebb0483e19e5109
Malintha Fernando <ccfernan@openrobotics.org>
2019-07-24 17:16:23 -0700

    adding rostest files

--- swarmsim_testing/test/gazebo_test.py
+++ swarmsim_testing/test/gazebo_test.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-PKG = 'swarmsim'
-import roslib; roslib.load_manifest(PKG)  # This line is not needed with Catkin.
+PKG = 'swarmsim_testing'
 
 import sys
 import unittest
+from sensor_msgs import NavSatFix
 
 ## A sample python unit test
 class TestBareBones(unittest.TestCase):
     ## test 1 == 1
     def test_one_equals_one(self): # only functions with 'test_'-prefix will be run!
+        image_msg = rospy.wait_for_message("/0/mavros/global_position/global", NavSatFix, 100)
         self.assertEquals(1, 1, "1!=1")
         print("## running test ##")
 
