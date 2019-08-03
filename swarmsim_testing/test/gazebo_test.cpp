34280d5b2a826dfd991091f88a176466a9c86776
Malintha Fernando <ccfernan@openrobotics.org>
2019-08-02 19:00:51 -0700

    add mavros state topic subscription

--- swarmsim_testing/test/gazebo_test.cpp
+++ swarmsim_testing/test/gazebo_test.cpp
@@ -1,46 +1,62 @@
 #include <gtest/gtest.h>
-#include <iostream>
-#include <vector>
 #include "ros/ros.h"
-#include "gazebo_msgs/ModelStates.h"
-#include "control_msgs/FollowJointTrajectoryFeedback.h"
-#include "geometry_msgs/Pose.h"
-#include "swarmsim/utils.h"
-#include "swarmsim/Trajectory.h"
 #include "testutils.h"
+#include "ros/console.h"
 
-using namespace std;
+TEST_F(TestUtils, testTwoRobots) {
 
-TEST(GazeboSimTestSuite, testTwoRobots) {
-    // TestUtils tu(2);
-    boost::shared_ptr<gazebo_msgs::ModelStates const> feedback;
-    feedback = ros::topic::waitForMessage<gazebo_msgs::ModelStates>("/gazebo/model_states",ros::Duration(100.0));
-    
-    // vector<Trajectory> tr_0 = tu.getAssertionValuesFromFile(0);
-    // vector<double> times_list = tu.getCumulativeTime(tr_0[0].tList);
-
-    // for(int i=0; i<times_list.size(); i++) {
-    //     double target_time = times_list[i];
-    //     ros::Time time = ros::Time::now();
-    //     double curr_sec = time.toSec();
-    //     do {
-    //         time = ros::Time::now();
-    //         curr_sec = time.toSec();
-    //     }
-    //     while(curr_sec < target_time);
-    //     cout<<"####assert position"<<endl;
-    //     // tu.assertPosition();
-
-    // }
+    mavrosStateSub = nh.subscribe("/1/mavros/state",1, &TestUtils::mavrosStateCB, (TestUtils*)this);
+    while(!guided) {
+        ros::Duration(0.5).sleep();
+        ros::spinOnce();
+    }
+    nh.getParam("/swarmsim_testing/trajDir_testing", trajDir);
+    nh.getParam("/swarmsim_testing/yamlFileName_testing", yamlFileName);
+    ROS_ERROR_STREAM("###file path: "<<yamlFileName);
 
 
-    // if(time.toSec() <= 0.5) {
-    //     geometry_msgs::Pose p1 = tu.getModelPose(0);
-    //     geometry_msgs::Pose p2 = tu.getModelPose(1);
+    ROS_DEBUG_STREAM("Drones are guided");
 
-    // }
-    // ASSERT_EQ(1, 1);
+
 }
+
+// TEST_F(TestUtils, testTwoRobots) {
+
+//     //subscribe to the last drone state
+
+//     ros::Duration(10).sleep();
+//     // boost::shared_ptr<gazebo_msgs::ModelStates const> feedback;
+//     // feedback = ros::topic::waitForMessage<gazebo_msgs::ModelStates>("/1/mavros/state",ros::Duration(100.0));
+//     // vector<Trajectory> tr_0 = tu.getAssertionValuesFromFile(0);
+//     // vector<double> times_list = tu.getCumulativeTime(tr_0[0].tList);
+//     // do {
+//     //     cout<<"waiting "<<endl;
+//     // }
+//     // while(!tu.readyDrones());
+//     // ROS_ERROR_STREAM("#####finishing+ "<<tu.readyDrones());
+
+//     // for(int i=0; i<times_list.size(); i++) {
+//     //     double target_time = times_list[i];
+//     //     ros::Time time = ros::Time::now();
+//     //     double curr_sec = time.toSec();
+//     //     do {
+//     //         time = ros::Time::now();
+//     //         curr_sec = time.toSec();
+//     //     }
+//     //     while(curr_sec < target_time);
+//     //     cout<<"####assert position"<<endl;
+//     //     // tu.assertPosition();
+
+//     // }
+
+
+//     // if(time.toSec() <= 0.5) {
+//     //     geometry_msgs::Pose p1 = tu.getModelPose(0);
+//     //     geometry_msgs::Pose p2 = tu.getModelPose(1);
+
+//     // }
+//     // ASSERT_EQ(1, 0);
+// }
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
--- swarmsim_testing/test/testutils.h
+++ swarmsim_testing/test/testutils.h
@@ -5,71 +5,96 @@
 #include "geometry_msgs/Pose.h"
 #include "swarmsim/utils.h"
 #include "swarmsim/Trajectory.h"
+#include "mavros_msgs/State.h"
+#include "ros/console.h"
+#include <gtest/gtest.h>
 
 using namespace std;
 
-class TestUtils {
+class TestUtils : public testing::Test {
 
     public:
-        TestUtils(int nDrones) {
-            ros::NodeHandle n("~");
-            modelStatesSub = n.subscribe("/gazebo/model_states", 10, &TestUtils::gazeboModelStateCB, this);
-            n.getParam("/swarmsim_testing/trajDir_testing", trajDir);
-            n.getParam("/swarmsim_testing/yamlFileName_testing", yamlFileName);
-            
+        ros::NodeHandle nh;
+        ros::Subscriber mavrosStateSub;
+        vector<vector<Trajectory> > goalset;
+        string trajDir;
+        string yamlFileName;
+        int nDrones;
+        ros::Subscriber modelStatesSub;
+        vector<geometry_msgs::Pose> pose_vec;
+        bool guided;
+
+        TestUtils() {
+            guided = false;
         }
 
         void gazeboModelStateCB(const gazebo_msgs::ModelStatesConstPtr& msg) {
             pose_vec = msg->pose;
         }
 
-        geometry_msgs::Pose getModelPose(int robotId) {
-            return pose_vec[robotId];
-        }
-
-        bool assertPosition(int posId) {
-            // for(int i=0;i<nDrones;i++) {
-                int i = 0; //hardcoding the drone num
-                vector<Eigen::Vector3d> targetPosList = goalset[i].pos;
-                Eigen::Vector3d targetPos = targetPosList[posId];
-                geometry_msgs::Pose robot_pose = pose_vec[2+i]; //first 2 elements of the list are asphalt plane and height map
-                Eigen::Vector3d currPos;
-                currPos << robot_pose.position.x, robot_pose.position.y, robot_pose.position.z;
-                return withinRadius(currPos, targetPos);
-            // }
-        }
-
-        bool withinRadius(Eigen::Vector3d p1, Eigen::Vector3d p2) {
-            return (p1 - p2).norm() < 1;
-        }
-
-        vector<double> getCumulativeTime(vector<double> tList) {
-            double cumulative = 0;
-            vector<double> cumulativeList;
-            for(int i=0;i<tList.size();i++) {
-                cumulative += tList[i];
-                cumulativeList.push_back(cumulative);
+        void mavrosStateCB(const mavros_msgs::StateConstPtr& msg) {
+            guided = msg->guided;
+            ROS_ERROR_STREAM("Mavros Guided CB");
+            if(guided) {
+                ROS_ERROR_STREAM("shutting down CB");
+                this->mavrosStateSub.shutdown();
             }
-            return cumulativeList;
         }
 
-        vector<Trajectory> getAssertionValuesFromFile(int horizon) {
+        // geometry_msgs::Pose getModelPose(int robotId) {
+        //     return pose_vec[robotId];
+        // }
+
+        // bool assertPosition(int posId) {
+        //     // for(int i=0;i<nDrones;i++) {
+        //         int i = 0; //hardcoding the drone num
+        //         vector<Eigen::Vector3d> targetPosList = goalset[i].pos;
+        //         Eigen::Vector3d targetPos = targetPosList[posId];
+        //         geometry_msgs::Pose robot_pose = pose_vec[2+i]; //first 2 elements of the list are asphalt plane and height map
+        //         Eigen::Vector3d currPos;
+        //         currPos << robot_pose.position.x, robot_pose.position.y, robot_pose.position.z;
+        //         return withinRadius(currPos, targetPos);
+        //     // }
+        // }
+
+        // bool withinRadius(Eigen::Vector3d p1, Eigen::Vector3d p2) {
+        //     return (p1 - p2).norm() < 1;
+        // }
+
+        // vector<double> getCumulativeTime(vector<double> tList) {
+        //     double cumulative = 0;
+        //     vector<double> cumulativeList;
+        //     for(int i=0;i<tList.size();i++) {
+        //         cumulative += tList[i];
+        //         cumulativeList.push_back(cumulative);
+        //     }
+        //     return cumulativeList;
+        // }
+
+        vector<Trajectory> getAssertionValuesFromFile(string yamlFilePath) {
+            // ROS_ERROR_STREAM("file path: "<<yamlFilePath);
+
             char cstr[yamlFileName.size() + 1];
             copy(yamlFileName.begin(), yamlFileName.end(), cstr);
             cstr[yamlFileName.size()] = '\0';
             int horizons;
             vector<Trajectory> goals;
-            this->goalset = goals;
-            simutils::processYamlFile(cstr, horizon, horizons, goals);
+            simutils::processYamlFile(cstr, 0, horizons, goals);
+            ROS_ERROR_STREAM("N_HORIZONS: "<<goalset.size());
+
+            for(int i=0;i<horizons;i++) {
+                simutils::processYamlFile(cstr, i, horizons, goals);
+                this->goalset.push_back(goals);
+            }
+            ROS_ERROR_STREAM("N_HORIZONS: "<<goalset.size());
+            
             return goals;
         }
 
-    private:
-        vector<Trajectory> goalset;
-        string trajDir;
-        string yamlFileName;
-        ros::NodeHandle n;
-        int nDrones;
-        ros::Subscriber modelStatesSub;
-        vector<geometry_msgs::Pose> pose_vec;
+        // bool readyDrones() {
+        //     return ready;
+        // }
+
+        // protected:
+
 };
