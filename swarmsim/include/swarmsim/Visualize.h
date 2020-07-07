80b19a086e2bb674bbf700d4d8b9bf9269d51510
Malintha Fernando <malinthak2@gmail.com>
2020-07-07 01:18:13 -0400

    add visualizing topology

--- swarmsim/include/swarmsim/Visualize.h
+++ swarmsim/include/swarmsim/Visualize.h
@@ -15,20 +15,33 @@ class Visualize {
         void addToPaths(vector<Trajectory> trajs);
         void draw();
         void addToGrid(std::vector<geometry_msgs::Point>);
+        void addStartGoal(std::vector<geometry_msgs::Point>);
+        void addAgentPaths(std::vector<std::vector<geometry_msgs::Point>>);
+        void addToTopo(std::vector<geometry_msgs::Point>);
+
 
     private:
         int ndrones;
+        int agents;
         ros::NodeHandle nh;
         string worldframe;
         ros::Publisher markerPub_traj;
         ros::Publisher markerPub_obs;
         ros::Publisher markerPub_samples;
+        ros::Publisher markerPub_paths;
+        ros::Publisher markerPub_topo;
 
         vector<visualization_msgs::Marker> marker_traj;
         vector<visualization_msgs::Marker> marker_obs;
+        vector<visualization_msgs::Marker> maker_agentPaths;
         visualization_msgs::Marker gridMarker;
+        visualization_msgs::Marker startMarker;
+        visualization_msgs::Marker topoMarker;
+
+
         string obstacleConfigFilePath;
         std::vector<Obstacle> obstacles;
+
         void initMarkers();
         std::vector<Obstacle> readObstacleConfig();
         void populateObstacles();
--- swarmsim/src/Visualize.cpp
+++ swarmsim/src/Visualize.cpp
@@ -11,6 +11,8 @@ Visualize::Visualize(ros::NodeHandle nh, string worldframe, int ndrones, string 
     this->markerPub_traj = nh.advertise<visualization_msgs::Marker>("visualization_marker/traj", 10);
     this->markerPub_obs = nh.advertise<visualization_msgs::Marker>("visualization_marker/obs", 10);
     this->markerPub_samples = nh.advertise<visualization_msgs::Marker>("visualization_marker/samples", 10);
+    this->markerPub_paths = nh.advertise<visualization_msgs::Marker>("visualization_marker/paths", 10);
+    this->markerPub_topo = nh.advertise<visualization_msgs::Marker>("visualization_marker/topo", 10); 
 }
 
 void Visualize::initMarkers() {
@@ -43,16 +45,13 @@ void Visualize::initMarkers() {
         m.color.g=0.7; 
         m.color.b=0.7;
         m.color.a = 1;
-
         m.pose.position.x = obstacles[i].center[0];
         m.pose.position.y = obstacles[i].center[1];
         m.pose.position.z = obstacles[i].center[2];
         ROS_DEBUG_STREAM("obs: "<<obstacles[i].center[0] <<" "<<obstacles[i].center[1]<<" "<<obstacles[i].center[2]);
-
         m.scale.x = obstacles[i].length;
         m.scale.y = obstacles[i].width;
         m.scale.z = obstacles[i].height;
-        
         m.pose.orientation.w = 1;
         this->marker_obs.push_back(m);
     }
@@ -67,18 +66,79 @@ void Visualize::initMarkers() {
         gridMarker.color.g=1; 
         gridMarker.color.b=1;
         gridMarker.color.a = 1;
-        gridMarker.scale.x = 0.1;
-        gridMarker.scale.y = 0.1;
-        gridMarker.scale.z = 0.1;
-
+        gridMarker.scale.x = 0.2;
+        gridMarker.scale.y = 0.2;
+        gridMarker.scale.z = 0.2;
         gridMarker.pose.orientation.w = 1;
+
+        //start/goal position markers
+        startMarker.header.stamp = ros::Time::now();
+        startMarker.type = visualization_msgs::Marker::CUBE_LIST;
+        startMarker.action = visualization_msgs::Marker::ADD;
+        startMarker.header.frame_id=this->worldframe;
+        startMarker.id = 2;
+        startMarker.color.r=1; 
+        startMarker.color.g=0.3; 
+        startMarker.color.b=0.3;
+        startMarker.color.a = 1;
+        startMarker.scale.x = 0.3;
+        startMarker.scale.y = 0.3;
+        startMarker.scale.z = 0.3;
+        startMarker.pose.orientation.w = 1;
+
+        // agent path marker
+        int nAgents = 2;
+        for(int i=0;i<nAgents; i++) {
+            visualization_msgs::Marker m;
+            m.header.stamp = ros::Time::now();
+            m.type = visualization_msgs::Marker::LINE_STRIP;
+            m.action = visualization_msgs::Marker::ADD;
+            m.header.frame_id=this->worldframe;
+            m.id = i;
+            m.color.r=0.7; 
+            m.color.g=0.7; 
+            m.color.b=1;
+            m.color.a = 1;
+            m.scale.x = 0.2;
+            m.pose.orientation.w = 1;
+            this->maker_agentPaths.push_back(m);
+        }
+        //marker for topology
+        topoMarker.header.stamp = ros::Time::now();
+        topoMarker.type = visualization_msgs::Marker::POINTS;
+        topoMarker.action = visualization_msgs::Marker::ADD;
+        topoMarker.header.frame_id=this->worldframe;
+        topoMarker.id = 5;
+        topoMarker.color.r=1; 
+        topoMarker.color.g=0.6; 
+        topoMarker.color.b=0.15;
+        topoMarker.color.a = 1;
+        topoMarker.scale.x = 0.3;
+        topoMarker.scale.y = 0.3;
+        topoMarker.scale.z = 0.3;
+        topoMarker.pose.orientation.w = 1;
+}
+
+void Visualize::addAgentPaths(std::vector<std::vector<geometry_msgs::Point>> schedule) {
+    std::cout<<"schedule: "<<schedule.size()<<std::endl;
+
+    for(int i=0;i<schedule.size(); i++) {
+        for(int p=0;p<schedule[i].size(); p++) {
+            maker_agentPaths[i].points.push_back(schedule[i][p]);
+        }
+    }
+}
+
+void Visualize::addToTopo(std::vector<geometry_msgs::Point> pts) {
+    for(int i=0; i<pts.size();i++) {
+        topoMarker.points.push_back(pts[i]);
+    }
 }
 
 void Visualize::addToPaths(vector<Trajectory> trajs) {
     for(int i=0; i < trajs.size(); i++) {
         Trajectory traj = trajs[i];
         ROS_DEBUG_STREAM("position list size: "<<traj.pos.size() << " " << traj.pos[0][0] << " " << traj.pos[0][1] << " " << traj.pos[0][2]);
-
         for (int p = 0; p < traj.pos.size(); p++) {
             geometry_msgs::Point pt;
             pt.x = traj.pos[p][0];
@@ -89,14 +149,27 @@ void Visualize::addToPaths(vector<Trajectory> trajs) {
     }
 }
 
+void Visualize::addStartGoal(std::vector<geometry_msgs::Point> pts) {
+    for(int i=0; i<pts.size();i++) {
+        startMarker.points.push_back(pts[i]);
+    }
+}
+
+
+
 void Visualize::draw() {
-    for(int i=0;i<this->ndrones;i++) {
-        markerPub_traj.publish(this->marker_traj[i]);
-    }
-    for(int i = 0; i<obstacles.size(); i++) {
-        markerPub_obs.publish(marker_obs[i]);
-    }
+    // for(int i=0;i<this->ndrones;i++) {
+    //     markerPub_traj.publish(this->marker_traj[i]);
+    // }
+    // for(int i = 0; i<obstacles.size(); i++) {
+    //     markerPub_obs.publish(marker_obs[i]);
+    // }
     markerPub_samples.publish(gridMarker);
+    markerPub_samples.publish(startMarker);
+    for(int i=0;i<this->maker_agentPaths.size();i++) {
+        markerPub_paths.publish(maker_agentPaths[i]);
+    }
+    markerPub_topo.publish(topoMarker);
 }
 
 void Visualize::addToGrid(std::vector<geometry_msgs::Point> ptsArray) {
